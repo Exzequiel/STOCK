3862fc9db7ed0ecfab01d107356b2d0628db341c
JohnMazua <johnmazua@unitec.edu>
2017-09-05 16:38:10 -0600

    Proveedor created

---
+++ CASMUL/Controllers/ProveedoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CASMUL.DB;
+
+namespace CASMUL.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        private dbcasmulEntities db = new dbcasmulEntities();
+
+        // GET: Proveedores
+        public ActionResult Index()
+        {
+            return View(db.proveedor.ToList());
+        }
+
+        // GET: Proveedores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            proveedor proveedor = db.proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // GET: Proveedores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Proveedores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_proveedor,nombre_proveedor,direccion,telefono,email,rtn,contacto,giro,razon_social,ciudad,activo")] proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.proveedor.Add(proveedor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(proveedor);
+        }
+
+        // GET: Proveedores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            proveedor proveedor = db.proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // POST: Proveedores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_proveedor,nombre_proveedor,direccion,telefono,email,rtn,contacto,giro,razon_social,ciudad,activo")] proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proveedor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(proveedor);
+        }
+
+        // GET: Proveedores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            proveedor proveedor = db.proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // POST: Proveedores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            proveedor proveedor = db.proveedor.Find(id);
+            db.proveedor.Remove(proveedor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
